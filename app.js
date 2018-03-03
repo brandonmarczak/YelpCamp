3010cdf9c4e6dae274dc7e1c5624cac571c26c4b
Brandon Marczak <brandonmarczak@yahoo.co.uk>
2018-03-03 16:37:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ console.log(process.env.DATABASEURL);
 
 //mongoose.connect(process.env.DATABASEURL);
 mongoose.connect("mongodb://localhost/yelp_camp_v11");
-//mongoose.connect("mongodb://brandon:monika666666@ds111876.mlab.com:11876/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
